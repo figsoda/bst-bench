e868a70b5ea9fc42d7e1bccf92124db3219bf307
figsoda <figsoda@pm.me>
2020-12-26 19:31:58 -0500

    use 64 bit unsigned integers

--- src/go/main.go
+++ src/go/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/emirpasic/gods/sets/treeset"
+	"github.com/emirpasic/gods/utils"
 )
 
 func main() {
-	set := treeset.NewWithIntComparator()
-	for i := 0; i < 1000000; i++ {
+	set := treeset.NewWith(utils.UInt64Comparator)
+	for i := uint64(0); i < 1000000; i++ {
 		set.Add(i)
 	}
 	fmt.Println(set.Size())
--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
 import qualified Data.Set as Set
+import Data.Word (Word64)
 
 main :: IO ()
-main = print $ Set.size $ foldl (flip Set.insert) Set.empty [0 .. 999999]
+main = print $ Set.size $ foldl
+  (flip Set.insert)
+  (Set.empty :: Set.Set Word64)
+  [0 .. 999999]
